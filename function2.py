b1a97eb76a2910011a310c60ebea3a2d492c415d
yj <dbswl9802@naver.com>
2023-08-21 16:49:34 +0900

    약간 수정하고 완료

--- function2.py
+++ function2.py
@@ -13,3 +13,21 @@ def func2(a):
 
 #호출
 print(func2(1))
+
+#기본값이 있는 함수
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드 인자 방식
+def connectURI(server, port):
+    strURL = "https://" + server +":"+ port
+    return strURL
+
+#호출
+print(connectURI("multicampus.com","80"))
+print(connectURI(port="80",server = "multicampus.com"))
