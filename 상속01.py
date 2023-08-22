56a50bc177c87fb1d4afc11e471d53dfefa17bb4
yj <dbswl9802@naver.com>
2023-08-22 16:21:59 +0900

    실행파일 만들기

--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,36 @@
+#부모 클래스 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, 
+            self.phoneNumber))
+    def working(self):
+        print("현재 작업중")
+    def coding(self):
+        print("현재 코딩중")
 
+#자식 클래스 정의
 class Student(Person):
+    #덮어쓰기(재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #덮어쓰기
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, 
+            self.phoneNumber))
+        print("Info(학과:{0}, 학번: {1})".format(self.subject, 
+            self.studentID))
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+s = Student("이순신", "010-111-1234", "빅데이터", "201234")
+p.printInfo()
+s.printInfo()
+s.working()
+s.coding()
 
 
